88386f763facd8c221b57cb67e8d2a6bcbc57414
Edzordzinam Alipui <edzordzinam.alipui@gmail.com>
2023-09-13 16:20:48 -0700

    Update

---
+++ pathss.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * line_up - Checks for existing files
+ * @namefile: Gives file names
+ *
+ * Return: An absolute route of the file or "NULL"
+ */
+
+char *line_up(char *namefile)
